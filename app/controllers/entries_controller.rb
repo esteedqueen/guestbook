4e4dfbf9d65db83c8fcd3be028d80d4df3a304e9
Esther Olatunde <olatundeesther@gmail.com>
2017-05-01 03:09:21 -0700

    add feature and controller spec for creating entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,29 @@
 class EntriesController < ApplicationController
+  # before_action :find_entry
   def index
-    
+    @entry = Entry.new
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.create(entry_params)
+    if @entry.save
+      redirect_to entries_path, notice: 'Entry successfully created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:name, :message)
+  end
+
+  def find_entry
+    @entry = Entry.find(params[:id])
   end
 end
---
+++ spec/features/user_creates_entry_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'User creates an entry' do
+  scenario 'successfully with valid data' do
+    visit root_path
+    expect{
+      fill_in 'Name', with: 'Esther'
+      fill_in "Message", with: 'Holla Holla Holla'
+      click_button 'Create Entry'
+    }.to change(Entry, :count).by(1)
+    entry = Entry.last
+    expect(entry).to_not be_nil
+    expect(current_path).to eq(entries_path)
+    expect(page).to have_content('successfully created')
+    # expect(page).to have_css 'table td', text: "Esther"
+  end
+
+  scenario 'unsuccessfully with invalid data' do
+    visit root_path
+    expect{
+      fill_in 'Name', with: 'Esther'
+      fill_in "Message", with: nil
+      click_button 'Create Entry'
+    }.to change(Entry, :count).by(0)
+    expect(current_path).to eq(entries_path)    
+    expect(page).to have_content("can't be blank")
+  end
+
+end
